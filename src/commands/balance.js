8ca333ea1518b5741ea4147614eaac6938a9a94e
U9G <winworkswow@gmail.com>
2021-01-07 09:31:11 -0500

    added bal

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,28 +1,28 @@
 const Discord = require('discord.js')
 const { allAcountsBusy, escapeMarkdown } = require('../util/discord-helper')
 
-// module.exports = {
-//   name: 'balance',
-//   cooldown: 5,
-//   aliases: ['bal'],
-//   description: "Get's a player's balance.",
-//   execute (message, args, accounts) {
-//     const acc = accounts.takeOne();
+module.exports = {
+  name: 'balance',
+  cooldown: 5,
+  aliases: ['bal'],
+  description: "Get's a player's balance.",
+  execute (message, args, accounts) {
+    const acc = accounts.takeOne()
 
-//     if (acc === null) {
-//       return message.channel.send(allAcountsBusy);
-//     }
-//     if (!args[0]) {
-//       return message.channel.send(createHelpEmbed());
-//     }
+    if (acc === null) {
+      return message.channel.send(allAcountsBusy)
+    }
+    if (!args[0]) {
+      return message.channel.send(createHelpEmbed())
+    }
 
-//     renderCommand(acc.bot, args[0]).then(embed => {
-//       message.channel.send(embed);
-//       acc.bot.removeAllListeners();
-//       acc.done();
-//     });
-//   }
-// };
+    renderCommand(acc.bot, args[0]).then(embed => {
+      message.channel.send(embed)
+      acc.bot.removeAllListeners()
+      acc.done()
+    })
+  }
+}
 
 function createHelpEmbed () {
   return new Discord.MessageEmbed()
@@ -65,7 +65,7 @@ function renderCommand (bot, ign) {
   function CreateEmbed (info) {
     return new Discord.MessageEmbed()
       .setAuthor('The Cosmic Sky Bot', 'https://i.ibb.co/7WnrkH2/download.png')
-      .setColor('RED')
+      .setColor('AQUA')
       .setTitle(`${escapeMarkdown(info.ign)} has $${info.balance}`)
   }
 }
