cdc9585232f1568c714d21a96de5de2722e7f0fd
U9G <winworkswow@gmail.com>
2021-03-05 15:10:58 -0500

    refactor alliance

--- src/commands/alliance.js
+++ src/commands/alliance.js
@@ -10,54 +10,36 @@ module.exports = {
   aliases: ['a'],
   race: true,
   description: 'Various alliance commands.',
-  execute (message, args, accounts) {
-    return new Promise((resolve, reject) => {
-      const subcommand = args[0]
-      const input = [args[1], args[2]]
-      if (subcommand === 'list') {
-        renderListCommand(accounts).then((embed) => {
-          resolve()
-          message.channel.send(embed)
-        })
-      } else if (subcommand === 'info' || subcommand === 'who') {
-        if (!input[0]) {
-          resolve()
-          message.channel.send(infoNoAllianceEmbed)
-        } else {
-          renderInfoCommand(accounts, input[0]).then((embed) => {
-            resolve()
-            message.channel.send(embed)
-          })
-        }
-      } else if (subcommand === 'level') {
-        if (!input[0]) {
-          resolve()
-          message.channel.send(levelNoAllianceEmbed)
-        } else {
-          renderLevelCommand(accounts, input[0]).then((embed) => {
-            resolve()
-            message.channel.send(embed)
-          })
-        }
-      } else if (subcommand === 'numbers') {
-        if (!input[0]) {
-          resolve()
-          message.channel.send(numbersNoAllianceEmbed)
-        } else {
-          resolve()
-          renderNumbersCommand(accounts, input[0]).then((embed) => {
-            message.channel.send(embed)
-          })
-        }
-      } else {
-        message.channel.send(helpEmbed)
-        resolve()
-      }
-    })
+  async execute (message, args, accounts) {
+    const subcommand = args[0]
+    const input = args[1]
+    let embed
+    switch (subcommand) {
+      case 'list':
+        embed = await renderListCommand(accounts)
+        break
+      case 'info':
+      case 'who':
+        if (!input) embed = infoNoAllianceEmbed()
+        else embed = await renderInfoCommand(accounts, input)
+        break
+      case 'level':
+        if (!input) embed = levelNoAllianceEmbed()
+        else embed = await renderLevelCommand(accounts, input)
+        break
+      case 'numbers':
+        if (!input) embed = numbersNoAllianceEmbed()
+        else embed = await renderNumbersCommand(accounts, input)
+        break
+      default:
+        embed = helpEmbed()
+        break
+    }
+    message.channel.send(embed)
   }
 }
 
-const helpEmbed = new Discord.MessageEmbed()
+const helpEmbed = () => new Discord.MessageEmbed()
   .setAuthor('The Cosmic Sky Bot', 'https://i.ibb.co/7WnrkH2/download.png')
   .setTitle('You forgot a subcommand for the >a command, try these:')
   .setDescription(
@@ -67,12 +49,12 @@ const helpEmbed = new Discord.MessageEmbed()
       '\n **·** `>a numbers [alliance/ign]` - gets number of players in an alliance and their truces/allies'
   )
   .setColor('AQUA')
-const infoNoAllianceEmbed = new Discord.MessageEmbed()
+const infoNoAllianceEmbed = () => new Discord.MessageEmbed()
   .setAuthor('The Cosmic Sky Bot', 'https://i.ibb.co/7WnrkH2/download.png')
   .setTitle('You forgot an alliance. Follow the format `>a who [alliance/ign]`')
-const levelNoAllianceEmbed = new Discord.MessageEmbed()
+const levelNoAllianceEmbed = () => new Discord.MessageEmbed()
   .setAuthor('The Cosmic Sky Bot', 'https://i.ibb.co/7WnrkH2/download.png')
   .setTitle('You forgot an alliance. Follow the format `>a level [alliance/ign]`')
-const numbersNoAllianceEmbed = new Discord.MessageEmbed()
+const numbersNoAllianceEmbed = () => new Discord.MessageEmbed()
   .setAuthor('The Cosmic Sky Bot', 'https://i.ibb.co/7WnrkH2/download.png')
   .setTitle('You forgot an alliance. Follow the format `>a numbers [alliance/ign]`')
